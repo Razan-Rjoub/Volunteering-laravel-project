2ae3c3e7918def008714ecfddc8f0964c9f3c1eb
ANFALDHERAT <anfaldherat7@gmail.com>
2023-09-08 22:09:23 +0300

    anfal

---
+++ resources/views/dashlayouts/main-sidebar.blade.php
@@ -0,0 +1,125 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="{{ asset('assets/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="{{ asset('assets/img/AdminLTELogo.png') }}" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Anfal dherat</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ asset('/table') }}" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Admins
+                </p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ asset('/Users') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Users</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ asset('/Categories') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Categories</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="{{ asset('/Donations') }}" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Donations</p>
+            </a>
+          </li>
+
+
+      <li class="nav-item">
+        <a href="{{ asset('/DonatedItems') }}" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>Donated Items</p>
+        </a>
+      </li>
+
+  <li class="nav-item">
+    <a href="{{ asset('/DonatedServices') }}" class="nav-link">
+      <i class="far fa-circle nav-icon"></i>
+      <p>Donated Services</p>
+    </a>
+
+</li>
+<li class="nav-item has-treeview menu-open">
+
+    <ul class="nav nav-treeview">
+      <li class="nav-item">
+        <a href="#" class="nav-link active">
+          <i class="far fa-circle nav-icon"></i>
+          <p>Form
+        </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="{{ asset('/DonationForm') }}" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>Donation Form</p>
+        </a>
+      </li>
+
+    </ul>
+  </li>
+  <li class="nav-item">
+    <a href="{{ asset('/DonatedItemsForm') }}" class="nav-link">
+      <i class="far fa-circle nav-icon"></i>
+      <p>Donated Items Form</p>
+    </a>
+  </li>
+
+
+
+
+<li class="nav-item">
+<a href="{{ asset('/DonatedServices') }}" class="nav-link">
+<i class="far fa-circle nav-icon"></i>
+<p>Donated Services</p>
+</a>
+
+</li>
+</ul>
+</li>
+
+
+
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DonationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -29,3 +27,80 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+
+
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('Home.index');
+});
+Route::get('/form', function () {
+    return view('Form');
+});
+Route::get('/aboutus', function () {
+    return view('aboutus.aboutus');
+})->name('aboutus');
+
+Route::get('/contactus', function () {
+    return view('Contact.contactus');
+})->name('contact');
+
+Route::get('/',[CategoryController::class,'index'])->name('home');
+Route::get('/donation',[DonationController::class,'index'])->name('donation');
+
+
+
+
+Route::get('/dash', function () {
+    return view('dashboardbage.home');
+});
+
+Route::get('/table', function () {
+    return view('dashboardbage.table');
+});
+
+Route::get('/Admins', function () {
+    return view('dashboardbage.Admins');
+});
+
+
+Route::get('/Users', function () {
+    return view('dashboardbage.Users');
+});
+
+Route::get('/Categories', function () {
+    return view('dashboardbage.Categories');
+});
+
+Route::get('/DonatedItems', function () {
+    return view('dashboardbage.DonatedItems');
+});
+
+
+Route::get('/DonatedItemsForm', function () {
+    return view('dashboardbage.DonatedItemsForm');
+});
+
+
+Route::get('/DonatedServices', function () {
+    return view('dashboardbage.DonatedServices');
+});
+
+
+Route::get('/DonatedServicesForm', function () {
+    return view('dashboardbage.DonatedServicesForm');
+});
+
+Route::get('/DonationForm', function () {
+    return view('dashboardbage.DonationForm');
+});
+
+Route::get('/Donations', function () {
+    return view('dashboardbage.Donations');
+});
+
