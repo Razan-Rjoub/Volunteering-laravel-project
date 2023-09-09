da7be6f9bbbc9ddb5d4ff2517c1f44262abb6012
razan alkateeb <razan.alkateeb0@gmail.com>
2023-09-10 00:52:56 +0300

    razan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Database\Seeders\CategorySeeder;
 use Database\Seeders\DonationSeeder;
 use Database\Seeders\ItemSeeder;
+use Database\Seeders\ServiceSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,6 +21,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             DonationSeeder::class,
+            ServiceSeeder::class,
+
             ItemSeeder::class,
         ]);
     }
